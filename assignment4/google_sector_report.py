34c7f2315e11374eec8140b823827f6429397053
Lindsey <lindseywoo@gmail.com>
2018-04-13 21:28:16 -0700

    added function to get change in sector value

--- assignment4/google_sector_report.py
+++ assignment4/google_sector_report.py
@@ -26,6 +26,20 @@ def get_file_data(file_name):
         return
 
 
+def get_sector_change(data):
+    sector_dict = {}
+    name = ""
+    x = data.find("div", class_="id-secperf sfe-section-major")
+    for link in x.find_all("tr"):
+        if not link.find("a") == None:
+            name = link.find("a").get_text()
+        if not link.find("span", class_="chg") == None:
+            sector_dict[name] = link.find("span", class_="chg").get_text()
+        if not link.find("span", class_="chr") == None:
+            sector_dict[name] = link.find("span", class_="chr").get_text()
+    return sector_dict
+
+
 def get_sector_data(data):
     sector_title = data.find(
         "div", class_="hdg top appbar-hide").get_text().strip()
@@ -45,21 +59,26 @@ def get_sector_data(data):
                     1].get_text().split("%)")[0].split("(")[1])
     sector_sorted = sorted((value, key)
                            for (key, value) in sector_dict.items())
-    sector_json = {}
-    sector_json[sector_title] = {"biggest_gainer": {
-        "equity": sector_sorted[len(sector_sorted)-1][1], "change": sector_sorted[len(sector_sorted)-1][0]}, "biggest_loser": sector_sorted[0][1], "change": sector_sorted[0][0]}
-    return sector_json
+    # sector_json = {}
+    return[sector_title, {"biggest_gainer": {
+        "equity": sector_sorted[len(sector_sorted)-1][1], "change": sector_sorted[len(sector_sorted)-1][0]}, "biggest_loser": sector_sorted[0][1], "change": sector_sorted[0][0]}]
+    # return sector_json
 
 
 def google_sector_report():
 
     try:
-        energy_data = get_file_data('Energy.htm')
-        print(get_sector_data(energy_data))
-        # finance_data = get_file_data('Google Finance.htm')
-        # industrial_data = get_file_data('Industrials.htm')
-        # get_sector_data(industrial_data)
-        # basic_mat_data = get_file_data('Basic Materials.htm')
+        import json
+        result = {}
+        energy = get_sector_data(get_file_data('Energy.htm'))
+        industrials = get_sector_data(get_file_data('Industrials.htm'))
+        basic = get_sector_data(get_file_data('Basic Materials.htm'))
+
+        result["result"] = {energy[0]: energy[1],
+                            industrials[0]: industrials[1], basic[0]: basic[1]}
+
+        # print(json.dumps(result))
+        get_sector_change(get_file_data('Google Finance.htm'))
     except:
         print("Unable to process HTML files")
 
