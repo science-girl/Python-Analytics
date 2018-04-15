d18621baa6fd7023010fb1acfa1416ff43cfdead
Lindsey <lindseywoo@gmail.com>
2018-04-14 21:43:21 -0700

    details for all links

--- job_hunt/glassdoor_scraper.py
+++ job_hunt/glassdoor_scraper.py
@@ -10,7 +10,15 @@ try:
     # print(data)
     jobs = data.find("div", {"id": "JobResults"}).find_all(
         "script")[0].get_text()
-    print(jobs)
+    import json
+    json_jobs = json.loads(jobs)
+    for job in json_jobs['itemListElement']:
+        print("---------------------------------------------------")
+        print(job["url"])
+        print("---------------------------------------------------")
+        detail_req = Request(job["url"], headers={'User-Agent': 'Mozilla/5.0'})
+        detail_data = BeautifulSoup(urlopen(detail_req).read(), 'lxml')
+        print(detail_data.find("div", class_="jobDescriptionContent"))
 
 except:
     print("Cannot connect to:", url)
