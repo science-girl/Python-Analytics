183314f1bdedcfc3f143ce5002eedc77fdb47269
Lindsey <lindseywoo@gmail.com>
2018-04-14 22:26:45 -0700

    refactored. details formatted and in english instead of french

--- job_hunt/glassdoor_scraper.py
+++ job_hunt/glassdoor_scraper.py
@@ -1,24 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+def get_details(jobs):
+    import json
+    json_jobs = json.loads(jobs)
+    for job in json_jobs['itemListElement']:
+        print("---------------------------------------------------")
+        detail_req = Request(job["url"].replace("fr.", ""), headers={
+                             'User-Agent': 'Mozilla/5.0'})
+        detail_data = BeautifulSoup(urlopen(detail_req).read(), 'lxml')
+        print(detail_data.find("title").get_text())
+        print(job["url"])
+        print("---------------------------------------------------")
+        print(detail_data.find("div", class_="jobDescriptionContent").get_text())
+
+
 url = 'https://www.glassdoor.ca/Job/vancouver-software-developer-jobs-SRCH_IL.0,9_IC2278756_KO10,28.htm'
 try:
     from urllib.request import Request, urlopen
 
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     data = BeautifulSoup(urlopen(req).read(), 'lxml')
-    # print(data)
     jobs = data.find("div", {"id": "JobResults"}).find_all(
         "script")[0].get_text()
-    import json
-    json_jobs = json.loads(jobs)
-    for job in json_jobs['itemListElement']:
-        print("---------------------------------------------------")
-        print(job["url"])
-        print("---------------------------------------------------")
-        detail_req = Request(job["url"], headers={'User-Agent': 'Mozilla/5.0'})
-        detail_data = BeautifulSoup(urlopen(detail_req).read(), 'lxml')
-        print(detail_data.find("div", class_="jobDescriptionContent"))
+    get_details(jobs)
 
 except:
     print("Cannot connect to:", url)
