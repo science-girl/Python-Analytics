591c87a7a4845a8b668820a027da94a8596baee9
Lindsey <lindseywoo@gmail.com>
2018-04-16 18:32:08 -0700

    fixed bug w/ duplicate links crawled

--- job_hunt/glassdoor_scraper.py
+++ job_hunt/glassdoor_scraper.py
@@ -3,26 +3,19 @@ import requests
 
 
 def get_details(jobs):
+    job_set = set()
     for job in jobs:
-        link = 'https://www.glassdoor.ca' + job.get('href')
-        print("---------------------------------------------------")
-        detail_req = Request(link, headers={
-            'User-Agent': 'Mozilla/5.0'})
-        detail_data = BeautifulSoup(urlopen(detail_req).read(), 'lxml')
-        print(detail_data.find("title").get_text())
-        print(link)
-        print("---------------------------------------------------")
-# import json
-# json_jobs = json.loads(jobs)
-# for job in json_jobs['itemListElement']:
-#     print("---------------------------------------------------")
-#     detail_req = Request(job["url"].replace("fr.", ""), headers={
-#                          'User-Agent': 'Mozilla/5.0'})
-#     detail_data = BeautifulSoup(urlopen(detail_req).read(), 'lxml')
-#     print(detail_data.find("title").get_text())
-#     print(job["url"].replace("fr.", ""))
-#     print("---------------------------------------------------")
-#     print(detail_data.find("div", class_="jobDescriptionContent").get_text())
+        job_set.add(job.get('href'))
+    for j in set(job_set):
+        if j.find('&jobListingId=') > 0:
+            link = 'https://www.glassdoor.ca' + j
+            detail_req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            detail_data = BeautifulSoup(urlopen(detail_req).read(), 'lxml')
+            print("---------------------------------------------------")
+            print(detail_data.find("title").get_text())
+            print(link)
+            print("---------------------------------------------------")
+            print(detail_data.find("div", class_="jobDescriptionContent").get_text())
 
 
 url = 'https://www.glassdoor.ca/Job/vancouver-software-developer-jobs-SRCH_IL.0,9_IC2278756_KO10,28'
@@ -30,7 +23,7 @@ try:
     from urllib.request import Request, urlopen
     import sys
     range_len = 4
-    for index in range(2, range_len, 1):
+    for index in range(2, range_len):
         job_url = url + '_IP' + str(index) + '.htm'
         print('JOB_URL', job_url)
         req = Request(job_url, headers={'User-Agent': 'Mozilla/5.0'})
