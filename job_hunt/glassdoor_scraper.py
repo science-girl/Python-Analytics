29850b89ec5e709765421739611d7470ee097f0a
Lindsey <lindseywoo@gmail.com>
2018-04-16 20:14:20 -0700

    added function to get number of pages to crawl

--- job_hunt/glassdoor_scraper.py
+++ job_hunt/glassdoor_scraper.py
@@ -18,12 +18,31 @@ def get_details(jobs):
             print(detail_data.find("div", class_="jobDescriptionContent").get_text())
 
 
+def get_num_pages(data):
+    try:
+        start = data.find("script").get_text().find('[') + 1
+        end = data.find("script").get_text().find(';') - 1
+        cleaned = data.find("script").get_text()[start:end].replace("\'", "\"")
+        import json
+        json_results = json.loads(cleaned.replace("\t", "").replace(
+            "\n", "").replace("[,", "["))
+        total_search_results = json_results['search']['totalSearchResults']
+        range_len = json_results['search']['displayedSearchResults']
+    except:
+        print("Error retrieving number of pages")
+        return
+    return int(int(total_search_results) / int(range_len))
+
+
 url = 'https://www.glassdoor.ca/Job/vancouver-software-developer-jobs-SRCH_IL.0,9_IC2278756_KO10,28'
 try:
     from urllib.request import Request, urlopen
     import sys
-    range_len = 4
-    for index in range(2, range_len):
+    req = Request(url + '.htm', headers={'User-Agent': 'Mozilla/5.0'})
+    data = BeautifulSoup(urlopen(req).read(), 'lxml')
+    num_pages = get_num_pages(data)
+
+    for index in range(2, num_pages):
         job_url = url + '_IP' + str(index) + '.htm'
         print('JOB_URL', job_url)
         req = Request(job_url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -36,5 +55,5 @@ try:
             e = sys.exc_info()[0]
             print("no details", e)
 except:
-    print("Cannot connect to:", job_url)
+    print("Cannot connect to:", url)
     print(sys.exc_info()[0])
